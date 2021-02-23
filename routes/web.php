c6bc4df7dfef0e0f9b15c6210f501e9e9ab758b7
gcg <guneycan@gmail.com>
2021-02-23 15:35:01 +0300

    use laravel new route method

--- routes/web.php
+++ routes/web.php
@@ -11,7 +11,10 @@ use Illuminate\Support\Facades\Route;
 | routes are loaded by the RouteServiceProvider within a group which
 | contains the "web" middleware group. Now create something great!
 |
-*/
+ */
 
-Route::get('/', 'PostController@index');
-Route::get('/posts/view/{post:slug}', 'PostController@view');
+
+use App\Http\Controllers\PostController;
+
+Route::get('/', [PostController::class, 'index']);
+Route::get('/posts/view/{post:slug}', [PostController::class, 'view']);
